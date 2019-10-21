28f79c0bb4c012eb7d5e87578135deb03e496401
Branum <jcb179@txstate.edu>
2019-10-21 15:24:38 -0500

    random polygon fuction add to map display

--- javascript/mapping.js
+++ javascript/mapping.js
@@ -6,3 +6,19 @@ var map = L.map('map').setView([29.8884, -97.9384], 14);
         attribution: '&copy; ' + mapLink + ' Contributors',
         maxZoom: 18,
         }).addTo(map);
+
+
+var pt = {
+  type: 'Feature',
+  properties:{name: "San Marcos"},
+  geometry: {
+    type: 'Point',
+    coordinates: [-97.9384, 29.8884]
+  }
+};
+
+var polygons = turf.randomPolygon(25, {bbox: [-180, -90, 180, 90]})
+L.geoJson(polygons, {color: "yellow"}).addTo(map);
+
+//pt = turf.randomPoint(25, {bbox: [-180, -90, 180, 90]});
+//L.geoJson(pt, {color: "green"}).addTo(map);
