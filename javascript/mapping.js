d911dff0ac25f48af71ace7a30a8449055aa9fe9
bradley-johnson <bdj42@txstate.edu>
2019-10-21 13:27:58 -0500

    Added turf.destination function to mapping.js

--- javascript/mapping.js
+++ javascript/mapping.js
@@ -6,3 +6,24 @@ var map = L.map('map').setView([29.8884, -97.9384], 14);
         attribution: '&copy; ' + mapLink + ' Contributors',
         maxZoom: 18,
         }).addTo(map);
+
+//Script to implement turf.destination
+var point;
+var distance;
+var bearing;
+var options;
+var coordinate;
+
+var map = L.map('map').setView([29.8884, -97.9384], 14);
+
+function destinationPoint(){
+  point = turf.point([-97.9414, 29.8833]);
+  distance = 2;
+  bearing = 120;
+  options = {units: 'miles'};
+
+  coordinate = turf.destination(point, distance, bearing, options);
+
+  L.geoJSON(point).addTo(map);
+  L.geoJSON(coordinate).addTo(map);
+};
