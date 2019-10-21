4ae7804e5d128ffb37fac33c2701d2b4fb7e7d6a
bradley-johnson <bdj42@txstate.edu>
2019-10-21 13:19:47 -0500

    Added turf.destination javascript code

--- javascript/mapping.js
+++ javascript/mapping.js
@@ -1,3 +1,9 @@
+var point;
+var distance;
+var bearing;
+var options;
+var coordinate;
+
 var map = L.map('map').setView([29.8884, -97.9384], 14);
     mapLink =
         '<a href="http://openstreetmap.org">OpenStreetMap</a>';
@@ -6,3 +12,15 @@ var map = L.map('map').setView([29.8884, -97.9384], 14);
         attribution: '&copy; ' + mapLink + ' Contributors',
         maxZoom: 18,
         }).addTo(map);
+
+function destinationPoint(){
+  point = turf.point([-97.9414, 29.8833]);
+  distance = 2;
+  bearing = 120;
+  options = {units: 'miles'};
+
+  coordinate = turf.destination(point, distance, bearing, options);
+
+  L.geoJSON(point).addTo(map);
+  L.geoJSON(coordinate).addTo(map);
+};
