29482c54c61faeeffd8bd34ddf27789e8d434f81
Dunlap <srd1@txstate.edu>
2019-10-21 15:59:13 -0500

    Added Turf Midpoint funciton

--- javascript/mapping.js
+++ javascript/mapping.js
@@ -6,3 +6,16 @@ var map = L.map('map').setView([29.8884, -97.9384], 14);
         attribution: '&copy; ' + mapLink + ' Contributors',
         maxZoom: 18,
         }).addTo(map);
+
+
+
+
+
+        var point1 = turf.point([-97, 29])
+         L.geoJson(point1).addTo(map);
+
+        var point2 = turf.point([-98, 28])
+        L.geoJson( point2).addTo(map);
+
+        var midpoint = turf.midpoint(point1, point2)
+        L.geoJson(midpoint).addTo(map);
